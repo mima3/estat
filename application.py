537689ad4ebc96af34e1c66a9997241fa847d8c1
m.itagaki2 <mima@js6.so-net.ne.jp>
2015-03-02 10:23:26 +0900

    範囲を制限

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ import cgi
 import urllib
 import estat_db
 import peewee
+import math
 
 
 app = Bottle()
@@ -29,6 +30,14 @@ def populationPage():
     return template('population').replace('\n', '')
 
 
+def str_isfloat(str):
+    try:
+        float(str)
+        return True
+    except ValueError:
+        return False
+
+
 @app.get('/json/get_population')
 def getPopulation():
     stat_id = request.query.stat_id
@@ -37,6 +46,22 @@ def getPopulation():
     nelat = request.query.nelat
     nelng = request.query.nelng
     attrval = request.query.attr_value
+
+    if (not str_isfloat(swlat) or
+        not str_isfloat(swlng) or
+        not str_isfloat(nelat) or
+        not str_isfloat(nelng)):
+        response.content_type = 'application/json;charset=utf-8'
+        response.set_header('Access-Control-Allow-Origin', '*')
+        response.status = 400
+        return json.dumps({'message':'wrong parameter type'})
+
+    maxrange = 1
+    if ((math.fabs(float(swlat) - float(nelat)) + math.fabs(float(swlng) - float(nelng))) > maxrange):
+        response.content_type = 'application/json;charset=utf-8'
+        response.set_header('Access-Control-Allow-Origin', '*')
+        response.status = 400
+        return json.dumps({'message':'wrong parameter range'})
 
     ret = estat_db.get_mesh_stat(stat_id, attrval, swlng, swlat, nelng, nelat)
     res = {'type': 'FeatureCollection', 'features': []}
@@ -59,6 +84,21 @@ def getPopulationGroupByMesh():
     swlng = request.query.swlng
     nelat = request.query.nelat
     nelng = request.query.nelng
+    if (not str_isfloat(swlat) or
+        not str_isfloat(swlng) or
+        not str_isfloat(nelat) or
+        not str_isfloat(nelng)):
+        response.content_type = 'application/json;charset=utf-8'
+        response.set_header('Access-Control-Allow-Origin', '*')
+        response.status = 400
+        return json.dumps({'message':'wrong parameter type'})
+
+    maxrange = 0.7
+    if ((math.fabs(float(swlat) - float(nelat)) + math.fabs(float(swlng) - float(nelng))) > maxrange):
+        response.content_type = 'application/json;charset=utf-8'
+        response.set_header('Access-Control-Allow-Origin', '*')
+        response.status = 400
+        return json.dumps({'message':'wrong parameter range'})
 
     ret = estat_db.get_mesh_stat_group_by_mesh(stat_id, swlng, swlat, nelng, nelat)
     res = {'type': 'FeatureCollection', 'features': []}
